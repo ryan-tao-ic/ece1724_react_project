8177a95c98bbcf7000af572bda861aa4f9751bf0
Luke Ren <luke.ren@mail.utoronto.ca>
2025-04-18 13:45:07 -0400

    combine multiple seed files to one

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-import { init } from "@/lib/init";
 
-init();
+// init();
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
